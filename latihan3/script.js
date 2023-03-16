f29118ed88143ed524a7f3685e03d577fd5f4ab8
Taufiq Pop <taufiqpop52@gmail.com>
2023-03-16 22:16:24 +0700

    latihan3

--- latihan3/script.js
+++ latihan3/script.js
@@ -1,42 +1,47 @@
 // Prototype
-
-// const methodMahasiswa = {
-//     makan : function (porsi) {
-//         this.energi += porsi;
-//         console.log(`Halo ${this.nama}, selamat makan!`);
-//     },
-    
-//     main : function (jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, selamat bermain!`);
-//     },
-    
-//     tidur : function (jam) {
-//         this.energi += jam * 2;
-//         console.log(`Halo ${this.nama}, selamat tidur!`);
-//     }
+// function Mahasiswa (nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
 // };
 
-function Mahasiswa (nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-};
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama} selamat makan!`
+// };
 
-Mahasiswa.prototype.makan = function (porsi) {
-    this.energi += porsi;
-    return `Halo ${this.nama} selamat makan!`
-};
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama} selamat main!`
+// };
 
-Mahasiswa.prototype.main = function (jam) {
-    this.energi -= jam;
-    return `Halo ${this.nama} selamat main!`
-};
+// Mahasiswa.prototype.tidur = function (jam) {
+//     this.energi += jam * 2;
+//     return `Halo ${this.nama} selamat tidur!`
+// };
 
-Mahasiswa.prototype.tidur = function (jam) {
-    this.energi += jam * 2;
-    return `Halo ${this.nama} selamat tidur!`
+
+// Versi Class (pembungkus prototype, dasarnya protopype dulu)
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;        
+        this.energi = energi;        
+    }
+
+    makan (porsi) {
+        this.energi += porsi;
+        return `Halo ${this.nama} selamat makan!`
+    };
+
+    main (jam) {
+        this.energi -= jam;
+        return `Halo ${this.nama} selamat main!`
+    };
+
+    tidur (jam) {
+        this.energi += jam * 2;
+        return `Halo ${this.nama} selamat tidur!`
+    }
 };
 
 let Lala = new Mahasiswa("Lala", 10);
-let Pop = new Mahasiswa("Pop", 9);
-
+let Pop = new Mahasiswa("Pop", 10);
