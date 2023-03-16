134f1295eb11a350d1ac5b64ea48579221d07b4b
Taufiq Pop <taufiqpop52@gmail.com>
2023-03-16 21:36:18 +0700

    latihan3

---
+++ latihan3/script.js
@@ -0,0 +1,29 @@
+// 2. Function Declaration (Lebih Efektif)
+// Object.create
+
+const methodMahasiswa = {
+    makan : function (porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan!`);
+    },
+    
+    main : function (jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat bermain!`);
+    },
+    
+    tidur : function (jam) {
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, selamat tidur!`);
+    }
+};
+
+function Mahasiswa (nama, energi) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    
+    return mahasiswa;
+}
+let Lala = Mahasiswa("Lala", 10);
+let Pop = Mahasiswa("Pop", 9);
