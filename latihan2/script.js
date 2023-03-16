971527267f9fcf8009252b6c89fb5fbe238ab359
Taufiq Pop <taufiqpop52@gmail.com>
2023-03-16 21:34:35 +0700

    latihan2-2

--- latihan2/script.js
+++ latihan2/script.js
@@ -1,21 +1,28 @@
-// 2. Function Declaration
-function Mahasiswa (nama, energi) {
-    let mahasiswa = {};
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// 2. Function Declaration (Lebih Efektif)
 
-    mahasiswa.makan = function (porsi) {
+const methodMahasiswa = {
+    makan : function (porsi) {
         this.energi += porsi;
         console.log(`Halo ${this.nama}, selamat makan!`);
-    }
-
-    mahasiswa.main = function (jam) {
+    },
+    
+    main : function (jam) {
         this.energi -= jam;
         console.log(`Halo ${this.nama}, selamat bermain!`);
+    },
+    
+    tidur : function (jam) {
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, selamat tidur!`);
     }
+};
 
+function Mahasiswa (nama, energi) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    
     return mahasiswa;
 }
-
 let Lala = Mahasiswa("Lala", 10);
 let Pop = Mahasiswa("Pop", 9);
