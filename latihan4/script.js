a587cca3404162b8e3e2e2162777c58cb255b383
Taufiq Pop <taufiqpop52@gmail.com>
2023-03-16 22:53:31 +0700

    latihan4

--- latihan4/script.js
+++ latihan4/script.js
@@ -1,4 +1,4 @@
-// 2.1 Closure (Execution Context, Hoisting, Scope)
+// 2.1 xecution Context, Hoisting, Scope
 
 // console.log(nama);
 // var nama = "Pop";
@@ -11,17 +11,29 @@
 // this = window
 
 // 2. Execution phase
-console.log(sayHello());
+// console.log(sayHello());
 
-var nama = "Pop";
-var umur = 21;
+// var nama = "Pop";
+// var umur = 21;
 
-function sayHello () {
-    return `Halo, nama saya ${nama}, saya umur ${umur} tahun.`;
-};
+// function sayHello () {
+//     return `Halo, nama saya ${nama}, saya umur ${umur} tahun.`;
+// };
 
 // function membuat Local Execution Context
 // yang di dalamnya terdapat creation dan execution phase
 // window
 // arguments
 // hoisting
+
+var nama = "Taufiq Pop";
+var username = "@taufiqpop99";
+
+function cetakURL () {
+    console.log(arguments);
+    var instagramURL = "https://instagram.com/";
+    return instagramURL + username;
+};
+
+// console.log(cetakURL(username));
+console.log(cetakURL("@lalapo19", "@ainunafi"));
