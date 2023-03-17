3f872efeabfe4e6907c1480f809d7c138c7e5481
Taufiq Pop <taufiqpop52@gmail.com>
2023-03-17 20:14:13 +0700

    latihan 7

--- latihan7/script.js
+++ latihan7/script.js
@@ -1,36 +1,11 @@
-// Arrow Function
-// const tampilNama = (nama) => {
-//     return `Halo, ${nama}`;
-// }
-// console.log(tampilNama("Taufiq Pop"));
+// Konsep THIS pada arrow function
 
 
-// 2 parameter
-// const tampilNama = (nama, waktu) => {
-//     return `Selamat ${waktu}, ${nama}`;
-// }
-// console.log(tampilNama("Lala", "Malam"));
+// constructor function
+const Mahasiswa = function () {
+    this.nama = "Taufiq Pop";
+    this.umur = 21;
+    console.log(this);
+}
 
-
-// Implisit Return (Lebih ringkas)
-// const tampilNama = nama => `Halo, ${nama}`;
-// console.log(tampilNama("Lala"));
-
-
-// Tanpa Parameter
-// const tampilNama = () => `Hello World!`;
-// console.log(tampilNama());
-
-
-// Contoh lain, pakai map
-let mahasiswa = ["Taufiq Pop", "Lala", "Nabila"];
-// let jumlahHuruf = mahasiswa.map(nama => nama.length);
-// console.log(jumlahHuruf);
-
-// jika object
-let jumlahHuruf = mahasiswa.map(nama => ({
-    nama: nama,
-    jmlHuruf: nama.length
-}));
-
-console.table(jumlahHuruf);
+const Pop = new Mahasiswa();
