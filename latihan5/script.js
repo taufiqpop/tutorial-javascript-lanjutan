df8a52cf8eb261e50218ab7fb9488a7c0ed3be37
Taufiq Pop <taufiqpop52@gmail.com>
2023-03-17 18:17:09 +0700

    latihan 5

--- latihan5/script.js
+++ latihan5/script.js
@@ -1,10 +1,48 @@
 // 2.2 Closure
-function init () {
-    let nama = "Taufiq Pop";
-    function tampilNama () {
-        console.log(nama);
-    }
-    tampilNama();
-};
+// function init () {
+//     // let nama = "Taufiq Pop";
+//     return function (nama) {
+//         console.log(nama);
+//     }
+// };
 
-init();
+// let panggilNama = init();
+// panggilNama("Lala");
+// panggilNama("Pop");
+
+
+
+
+// Contoh 2
+// function ucapkanSalam (waktu) {
+//     return function (nama) {
+//         console.log(`Halo ${nama}, Selamat ${waktu}, Semoga Harimu Menyenangkan!`);
+//     };
+// };
+
+// let selamatPagi = ucapkanSalam("Pagi");
+// let selamatSiang = ucapkanSalam("Siang");
+// let selamatMalam = ucapkanSalam("Malam");
+
+// console.dir(selamatMalam("Lala"));
+
+
+
+// Contoh 3
+let add = (function () {
+    let counter = 0;
+    return function () {
+        return ++counter;
+    }
+})();
+
+counter = 100;
+
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
