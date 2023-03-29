885d34ab8ee2305536f551a3d424efd1b2b7e712
taufiqpop99 <taufiqpop52@gmail.com>
2023-03-29 23:13:35 +0700

    error handling

--- latihan19/error-handling.js
+++ latihan19/error-handling.js
@@ -1,10 +1,39 @@
 // Error Handling
 const searchButton = document.querySelector('.search-button');
 searchButton.addEventListener('click', async function () {
-    const inputKeyword = document.querySelector('.input-keyword');
-    const movies = await getMovies(inputKeyword.value);
-    updateUI(movies);
+    try {
+        const inputKeyword = document.querySelector('.input-keyword');
+        const movies = await getMovies(inputKeyword.value);
+        updateUI(movies);
+    }
+    catch (error) {
+        // console.log(error);
+        alert(error);
+    }
 });
+
+function getMovies (keyword) {
+    return fetch('http://www.omdbapi.com/?apikey=e36e783b&s=' + keyword)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(response => {
+            if (response.Response === 'False') {
+                throw new Error(response.Error)
+            }
+            return response.Search;
+        });
+}
+
+function updateUI (movies) {
+    let cards = '';
+    movies.forEach(m => cards += showCards(m));
+    const movieContainer = document.querySelector('.movie-container');
+    movieContainer.innerHTML = cards;
+}
 
 // Event Binding
 document.addEventListener('click', async function (e) {
@@ -14,19 +43,6 @@ document.addEventListener('click', async function (e) {
         updateUIDetail (movieDetail);
     }
 });
-
-function getMovies (keyword) {
-    return fetch('http://www.omdbapi.com/?apikey=e36e783b&s=' + keyword)
-        .then(response => response.json())
-        .then(response => response.Search);
-}
-
-function updateUI (movies) {
-    let cards = '';
-    movies.forEach(m => cards += showCards(m));
-    const movieContainer = document.querySelector('.movie-container');
-    movieContainer.innerHTML = cards;
-}
 
 function getMovieDetail (imdbid) {
     return fetch('http://www.omdbapi.com/?apikey=e36e783b&i=' + imdbid)
