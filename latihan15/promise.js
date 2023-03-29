d0d874fb80ac353e6f3933fbf2ed60639533f8a9
taufiqpop99 <taufiqpop52@gmail.com>
2023-03-29 12:58:25 +0700

    latihan15

--- latihan15/promise.js
+++ latihan15/promise.js
@@ -35,16 +35,74 @@
 // aksi (then / catch)
 
 // Contoh 1 (sederhana)
-let ditepati = true;
-const janji1 = new Promise((resolve, reject) => {
-    if (ditepati) {
-        resolve('Janji telah ditepati!');
-    }
-    else {
-        reject('Ingkar janji..');
-    }
+// let ditepati = true;
+// const janji1 = new Promise((resolve, reject) => {
+//     if (ditepati) {
+//         resolve('Janji telah ditepati!');
+//     }
+//     else {
+//         reject('Ingkar janji..');
+//     }
+// });
+
+// janji1
+//     .then(response => console.log('OK! : ' + response))
+//     .catch(response => console.log('NOT OK! : ' + response));
+
+
+// Contoh 2
+// let ditepati = true;
+// const janji2 = new Promise((resolve, reject) => {
+//     if (ditepati) {
+//         setTimeout(() => {
+//             resolve('Ditepati setelah beberapa waktu!');
+//         }, 2000);
+//     }
+//     else {
+//         setTimeout(() => {
+//             resolve('Tidak ditepati setelah beberapa waktu!');
+//         }, 2000);
+//     }
+// });
+
+// console.log('mulai');
+// // console.log(janji2.then(() => console.log(janji2))); // biar tau proses pendingnya
+// janji2
+//     .finally(() => console.log('selesai menunggu!'))
+//     .then(response => console.log('OK! : ' + response))
+//     .catch(response => console.log('NOT OK! : ' + response));
+// console.log('selesai');
+
+
+// Promise.all()
+const film = new Promise(resolve => {
+    setTimeout(() => {
+        resolve([{
+            judul: 'Paddle Pop',
+            sutradara: 'Taufiq Pop',
+            pemeran: 'Professor, Liona'
+        }])
+    }, 1000)
 });
 
-janji1
-    .then(response => console.log('OK! : ' + response))
-    .catch(response => console.log('NOT OK! : ' + response));
+const cuaca = new Promise(resolve => {
+    setTimeout(() => {
+        resolve([{
+            kota: 'Klaten',
+            temp: 25,
+            kondisi: 'Berawan'
+        }])
+    }, 500)
+});
+
+// film.then(response => console.log(response));
+// cuaca.then(response => console.log(response));
+
+Promise.all([film, cuaca])
+    // .then(response => console.log(response)); // Array Digabung
+    .then(response => { // Spread Operator (Memecah Array)
+        const [film, cuaca] = response;
+        console.log(film);
+        console.log(cuaca);
+
+    });
